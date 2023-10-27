8177270bd9d65ec44aa7feb3a50526795db5a073
unknown <anthonychi36@gmail.com>
2023-10-26 23:08:51 -0700

    Add WPM and acuracy calculation to be displayed after completion of a test and a retry button

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const wordsContainer = document.getElementById("words-container");
 const words = document.getElementById("words");
 const inputField = document.getElementById("input-field");
+const wpmCounter = document.getElementById("wpm-counter");
+const retryButton = document.getElementById("retry-button");
+const accuracyMeter = document.getElementById("accuracy");
 let typed = "";
+let started = false;
+let completed = false;
+let startTime;
 
-const wordStrings = ["string test sentence", "this is also a string", "hello world"];
+
+const wordStrings = [
+    "the quick brown fox jumps over the lazy dog",
+    "sentence words strings characters keyboard",
+    "the sun is setting over the horizon painting the sky with shades of orange and pink",
+    "a gentle breeze rustled the leaves of the trees creating a soothing melody"
+];
 let chosenWord = wordStrings[0];
 
 generateSpanOfWords(chosenWord);
@@ -24,49 +36,112 @@ function generateSpanOfWords(wordString){
 }
 
 function updateText(){
+    if (!started){
+        started = true;
+        startTime = new Date();
+    }
     // const chosenWords = chosenWord.split(" ");
     const chosenWords = chosenWord;
     // const typedWords = inputField.value.split(" "); 
     const typedWords = inputField.value;
 
-    if (chosenWords.length < typedWords.length){ // Case: too much characters typed
-        const span = document.createElement("span");
-        span.textContent = typedWords.charAt(typedWords.length-1);
-        span.className = "superwrong";
-        words.appendChild(span);
+    // if (chosenWords.length < typedWords.length){
+    //     const span = document.createElement("span");
+    //     span.textContent = typedWords.charAt(typedWords.length-1);
+    //     span.className = "superwrong";
+    //     words.appendChild(span);
+    // }
+
+    if (typedWords.length > 0){
+        const lastWord = typedWords[typedWords.length-1];
+        const lastChosenWord = chosenWords[typedWords.length-1];
+
+        const typedCharacter = lastWord.charAt(lastWord.length-1); 
+        const chosenCharacter = lastChosenWord.charAt(lastWord.length-1);
+
+        const lastSpan = words.children[inputField.value.length-1];
+
+        if (typedCharacter == chosenCharacter){
+            lastSpan.classList.add("correct");
+        }
+        else{
+            lastSpan.classList.add("wrong");
+        }
+
+        if (!containsIncompleteSpan()){
+            completed = true;
+
+            const endTime = new Date();
+
+            const elapsedTime = (endTime - startTime)/1000;
+            const elapsedTimeInMinutes = elapsedTime/60;
+
+            const wpm = (chosenWord.length/5)/elapsedTimeInMinutes;
+
+            console.log("Time taken: " + elapsedTime + " seconds");
+            console.log("WPM: " + Math.floor(wpm) + " wpm");
+            console.log("Accuracy: " + calculateAccuracy() + "%");
+
+            wpmCounter.classList.remove("hidden");
+            retryButton.classList.remove("hidden");
+            accuracyMeter.classList.remove("hidden");
+
+            wpmCounter.textContent = "WPM: " + Math.round(wpm);
+            accuracyMeter.textContent = "Accuracy: " + Math.round(calculateAccuracy()) + "%";
+            
+        }
+    }   
+    updateActivePosition(); // Moves the underline to the correct location
+}
+
+function resetAll(){
+    started = false;
+    completed = false;
+    chosenWord = wordStrings[Math.floor(Math.random() * wordStrings.length)];
+    inputField.value = "";
+
+    while (words.firstChild) {
+        words.removeChild(words.firstChild);
+    }
+    wpmCounter.classList.add("hidden");
+    retryButton.classList.add("hidden");
+    accuracyMeter.classList.add("hidden");
+    generateSpanOfWords(chosenWord);
+}
+
+function calculateAccuracy(){
+    const array = words.children;
+    const totalCharacters = words.children.length;
+    let correctCharacters = 0;
+
+    for (let i = 0; i < totalCharacters; i++){
+        if (array[i].classList.contains("correct")){
+            correctCharacters++;
+        }
     }
 
-    const lastWord = typedWords[typedWords.length-1];
-    const lastChosenWord = chosenWords[typedWords.length-1];
-
-    const typedCharacter = lastWord.charAt(lastWord.length-1); 
-    const chosenCharacter = lastChosenWord.charAt(lastWord.length-1);
-
-    const lastSpan = words.children[inputField.value.length-1];
-
-    if (typedCharacter == chosenCharacter){
-        lastSpan.classList.add("correct");
-    }
-    else{
-        lastSpan.classList.add("wrong");
-    }
-
-    if (!containsIncompleteSpan()){
-        console.log("Test completed!");
-    }
-
-    updateActivePosition();
+    return (correctCharacters/totalCharacters * 100);
 }
 
 function updateActivePosition(){
-    const array = Array.from(words.children);
+    const array = words.children;
 
-    array.forEach(element => {
-        if (element.classList.contains("active")){
-            element.classList.remove("active");
+    // array.forEach(element => {
+    //     if (element.classList.contains("active")){
+    //         element.classList.remove("active");
+    //     }
+    // })
+    for (let i = 0; i < words.children.length; i++){
+        if (array[i].classList.contains("active")){
+            array[i].classList.remove("active");
         }
-    })
-    array[inputField.value.length].classList.add("active");
+        if (i >= inputField.value.length){
+            array[i].classList.remove("correct", "wrong");
+        }
+    }
+    if (array.length > inputField.value.length){
+        array[inputField.value.length].classList.add("active");
+    }
 }
 
 function containsIncompleteSpan(){
@@ -80,7 +155,15 @@ function containsIncompleteSpan(){
     return containsIncomplete;
 }
 
-document.addEventListener("keydown", () => inputField.focus());
+document.addEventListener("keydown", (event) => {
+    inputField.focus();
+    if (event.key === "Enter"){
+        if (completed){
+            retryButton.click();
+        }
+    }
+});
 inputField.addEventListener("input", updateText);
+retryButton.addEventListener("click", resetAll);
 
 updateActivePosition();
